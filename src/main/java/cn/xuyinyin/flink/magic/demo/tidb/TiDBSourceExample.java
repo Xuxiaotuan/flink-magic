d72a8408311d8bee4415f49bbf7f627c98263558
Xuxiaotuan <jia_yangchen@163.com>
2023-08-07 13:39:51 +0800

    scala not ready

---
+++ src/main/java/cn/xuyinyin/flink/magic/demo/tidb/TiDBSourceExample.java
@@ -0,0 +1,71 @@
+package cn.xuyinyin.flink.magic.demo.tidb;
+
+import com.ververica.cdc.connectors.tidb.TDBSourceOptions;
+import com.ververica.cdc.connectors.tidb.TiDBSource;
+import com.ververica.cdc.connectors.tidb.TiKVChangeEventDeserializationSchema;
+import com.ververica.cdc.connectors.tidb.TiKVSnapshotEventDeserializationSchema;
+import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
+import org.apache.flink.util.Collector;
+import org.tikv.kvproto.Cdcpb;
+import org.tikv.kvproto.Kvrpcpb;
+
+import java.util.HashMap;
+/**
+ * @author : XuJiaWei
+ * @since : 2023-08-07 11:44
+ */
+
+
+public class TiDBSourceExample {
+
+    public static void main(String[] args) throws Exception {
+
+        SourceFunction<String> tidbSource =
+                TiDBSource.<String>builder()
+                        .database("test") // set captured database
+                        .tableName("test") // set captured table
+                        .tiConf(
+                                TDBSourceOptions.getTiConfiguration(
+                                        "192.168.11.62:2379", new HashMap<>()))
+                        .snapshotEventDeserializer(
+                                new TiKVSnapshotEventDeserializationSchema<String>() {
+                                    @Override
+                                    public void deserialize(
+                                            Kvrpcpb.KvPair record, Collector<String> out)
+                                            throws Exception {
+                                        out.collect(record.toString());
+                                    }
+
+                                    @Override
+                                    public TypeInformation<String> getProducedType() {
+                                        return BasicTypeInfo.STRING_TYPE_INFO;
+                                    }
+                                })
+                        .changeEventDeserializer(
+                                new TiKVChangeEventDeserializationSchema<String>() {
+                                    @Override
+                                    public void deserialize(
+                                            Cdcpb.Event.Row record, Collector<String> out)
+                                            throws Exception {
+                                        out.collect(record.toString());
+                                    }
+
+                                    @Override
+                                    public TypeInformation<String> getProducedType() {
+                                        return BasicTypeInfo.STRING_TYPE_INFO;
+                                    }
+                                })
+                        .build();
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        // enable checkpoint
+        env.enableCheckpointing(3000);
+        env.addSource(tidbSource).print().setParallelism(1);
+
+        env.execute("Print TiDB Snapshot + Binlog");
+    }
+}
---
+++ src/main/scala/cn.xuyinyin.flink.magic.demo/mysql/MysqlCDC.scala
@@ -0,0 +1,13 @@
+package cn.xuyinyin.flink.magic.demo.mysql
+
+/**
+ * @author : XuJiaWei
+ * @since : 2023-08-05 14:13
+ */
+object MysqlCDC {
+
+  def main(args: Array[String]): Unit = {
+    println("mysql cdc")
+  }
+
+}
