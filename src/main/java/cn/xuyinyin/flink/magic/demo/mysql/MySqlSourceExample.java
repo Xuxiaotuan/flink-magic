714485ee597e5e403f5dfcb8d5aeae63c5c4530e
Xuxiaotuan <jia_yangchen@163.com>
2023-08-07 13:49:36 +0800

    mysql error

---
+++ src/main/java/cn/xuyinyin/flink/magic/demo/mysql/MySqlSourceExample.java
@@ -0,0 +1,39 @@
+package cn.xuyinyin.flink.magic.demo.mysql;
+
+import com.ververica.cdc.connectors.mysql.source.MySqlSource;
+import com.ververica.cdc.debezium.JsonDebeziumDeserializationSchema;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * @author : XuJiaWei
+ * @since : 2023-08-05 14:27
+ */
+
+
+public class MySqlSourceExample {
+    public static void main(String[] args) throws Exception {
+        MySqlSource<String> mySqlSource = MySqlSource.<String>builder()
+                .hostname("192.168.11.62")
+                .port(3309)
+                .databaseList(".*") // 设置捕获的数据库， 如果需要同步整个数据库，请将 tableList 设置为 ".*".
+                .tableList("yourDatabaseName.yourTableName") // 设置捕获的表
+                .username("root")
+                .password("root")
+                .scanNewlyAddedTableEnabled(true)
+                .deserializer(new JsonDebeziumDeserializationSchema()) // 将 SourceRecord 转换为 JSON 字符串
+                .build();
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        // 设置 3s 的 checkpoint 间隔
+        env.enableCheckpointing(3000);
+
+        env.fromSource(mySqlSource, WatermarkStrategy.noWatermarks(), "MySQL Source")
+                // 设置 source 节点的并行度为 4
+                .setParallelism(4)
+                .print().setParallelism(1); // 设置 sink 节点并行度为 1
+
+        env.execute("Print MySQL Snapshot + Binlog");
+    }
+}
